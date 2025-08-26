04d3ded37fa91fac3c70cda1c202b89762149dad
jrdesignerofc <jrdesignerofc@gmail.com>
2025-08-25 23:37:46 -0300

    chore: mapear alias @ para a raiz no webpack do Next

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+// next.config.js
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    turbo: { rules: {} }, // ok deixar vazio
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname), // permite import '@/...' apontando pra raiz
+    };
+    return config;
   },
 };
 
